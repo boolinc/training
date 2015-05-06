0f649ca71579bd42231b4051744d50896301574e
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-06 01:08:34 -0500

    Add conditionals sketch

---
+++ js-basic/classes/conditionals/index.js
@@ -0,0 +1,18 @@
+'use strict';
+
+/**
+ *
+ * Nota: Para imprimir algo, se usa la función
+ * console.log()
+ *
+ * TODO: Buscar qué es console.log()
+ *
+ */
+
+var edad = /* Ingrese su edad en números */;
+
+if(/* edad es mayor o igual a 15 */){
+    // Imprima "Soy el mejor"
+} else {
+    // Imprima "No, alguien más es el mejor"
+}
