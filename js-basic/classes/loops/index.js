90c2853ad44da47e75a9d68f6f6ee79fd7d16e73
Pablo Andrés Dorado Suárez <pandres95@boolinc.co>
2015-05-22 23:05:12 -0500

    Set TODO

---
+++ js-basic/classes/loops/index.js
@@ -0,0 +1,7 @@
+"use strict";
+
+/**
+ * TODO: Crear un ciclo donde busque un número entre el 1 y el 100 que sea
+ * mayor que el número primo más alto entre 1 y 100
+ * TODO: Sumar números entre 1 y 100
+ */
