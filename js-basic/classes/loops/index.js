d80c265b4efa3e6f80ac3a6436e3e686cb123118
Javier Collazos <jcollazosgomez@gmail.com>
2015-05-23 00:27:57 -0500

    ciclos y objetos en index.js

--- js-basic/classes/loops/index.js
+++ js-basic/classes/loops/index.js
@@ -5,3 +5,44 @@
  * mayor que el número primo más alto entre 1 y 100
  * TODO: Sumar números entre 1 y 100
  */
+
+
+for( var i = 1; i <= 100 ; i++)
+{
+    if( i == 97)
+    {
+        console.log(i);
+    }
+}
+
+function sumatoria()
+{
+    var suma = 0;
+
+    for( var i = 1; i <= 100 ; i++)
+    {
+        suma = suma + i;
+    }
+    return suma;
+}
+
+function primos(callback)
+{
+    for( var i = 1; i <=100 ; i++)
+    {
+        if( i == 97)
+        {
+            callback(i);
+        }
+    }
+}
+
+module.exports - {
+    primos: primos,
+    suma: sumatoria
+};
+
+
+//crear objeto que incluya como propiedades esos dos funciones.
+
+//PERFECT :v
