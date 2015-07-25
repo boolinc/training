a18caa48d043761ef5502cf88733634cf9440999
Javier Collazos <jcollazosgomez@gmail.com>
2015-07-24 20:43:00 -0500

    come back to platzi

--- Programacion Basica/canvas/canvas.js
+++ Programacion Basica/canvas/canvas.js
@@ -17,24 +17,42 @@ function inicio()
     lienzo.beginPath();
     lienzo.strokeStyle = "#00F";
     lienzo.arc(150,150,100, (Math.PI *2), false);
+    lienzo.fillStyle = "#00F";
+    lienzo.fill();
     lienzo.closePath();
     lienzo.stroke();
 
-    dibujarGrilla(lienzo);
 }
 
-function dibujarGrilla (l) {
-
+function dibujarGrilla (l)
+{
     var ancho = 300, alto = 300;
     var linea;
-    for(linea = 0 ; linea < 30 ; linea ++)
+    var anchoLinea = 30;
+    var limiteX = ancho / anchoLinea;
+    var limiteY = alto / anchoLinea;
+    l.strokeStyle = "#AAA";
+
+    for(linea = 0 ; linea <= 20 ; linea ++)
     {
+        punto = linea * anchoLinea;
         l.beginPath();
-        l.strokeStyle = "#AAA";
-        l.moveTo(linea * 10 , 0);
-        l.lineTo(linea * 10 , 300);
-        l.closePath();
+        l.moveTo(punto , 0);
+        l.lineTo(0 , punto);
         l.stroke();
+        l.closePath();
+    }
+
+
+
+    //for(linea = 0 ; linea <= limiteY ; linea ++)
+    {
+    //    punto = linea * anchoLinea
+        l.beginPath();
+    //    l.moveTo(0 , punto);
+    /    l.lineTo(punto , 0);
+        //l.stroke();
+        l.closePath();
     }
 
 }
