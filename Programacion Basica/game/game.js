81249c4b3eefc1c81ac6bcb27819a8a2ab92bd52
Javier Collazos <jcollazosgomez@gmail.com>
2015-07-25 12:53:14 -0500

    La venganza de Diana

--- Programacion Basica/game/game.js
+++ Programacion Basica/game/game.js
@@ -1,17 +1,56 @@
 var tablero;
+
 var fondo = {
-    imagenURL:"fondo.png"
+    imagenURL:"fondo.png",
+    imagenOK: false
 };
+
+var tifis = {
+    x: 100,
+    y: 100,
+    frenteURL: "diana-frente.png",
+    frenteOK: false
+};
+
 function inicio()
 {
-    var canvas = document.getElementById("game");
+    var canvas = document.getElementById("campo");
     tablero = canvas.getContext("2d");
+
     fondo.imagen = new Image();
     fondo.imagen.src = fondo.imagenURL;
-    fondo.imagen.onload = dibujarFondo;
+    fondo.imagen.onload = confirmarFondo;
 
+    tifis.frente = new Image();
+    tifis.frente.src = tifis.frenteURL;
+    tifis.frente.onload = confirmarFrente;
+
+    var m = document.getElementById("mover");
+    m.addEventListener("click", movimiento);
 }
-function dibujarFondo()
+function confirmarFondo()
 {
-    tablero.drawImage(fondo.imagen, 0, 0);
+    fondo.imagenOK = true;
+    dibujar();
+}
+function confirmarFrente()
+{
+    tifis.frenteOK = true;
+    dibujar();
+}
+function dibujar()
+{
+    if(fondo.imagenOK == true)
+    {
+        tablero.drawImage(fondo.imagen, 0,0);
+    }
+    if(tifis.frenteOK == true)
+    {
+        tablero.drawImage(tifis.frente, tifis.x,tifis.y);
+    }
+}
+function movimiento()
+{
+    tifis.x += 10;
+    dibujar();
 }
