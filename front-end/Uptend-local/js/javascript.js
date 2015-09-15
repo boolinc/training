b8f072a09d18f5b5bb1a48f02112de48ef8d13e3
Javier Collazos <jcollazosgomez@gmail.com>
2015-09-15 18:10:03 -0500

    uptend local fase 1

---
+++ front-end/Uptend-local/js/javascript.js
@@ -0,0 +1,16 @@
+/*var name = document.getElementById("x").value;
+var email = document.getElementById("y").value;*/
+
+
+/*function borrar() {
+  name.write('.');
+  email.write('.');
+}*/
+
+function yolo() {
+  alert ("Muchas gracias, eres lo mejor!");
+  var x = document.getElementById("x").value;
+  var y = document.getElementById("y").value;
+  console.log(x,y);
+  /*borrar();*/
+}
